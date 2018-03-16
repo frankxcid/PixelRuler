8f9bc4e4e94428b7277d36667673e86c04106902
Frank <frank.cedeno@nelsonstud.com>
2018-03-16 09:37:14 -0400

    Add project files.

---
+++ PixelRuler/frmMain.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace PixelRuler
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.iLstAspect = new System.Windows.Forms.ImageList(this.components);
+            this.btnAspect = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.ttAll = new System.Windows.Forms.ToolTip(this.components);
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // iLstAspect
+            // 
+            this.iLstAspect.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("iLstAspect.ImageStream")));
+            this.iLstAspect.TransparentColor = System.Drawing.Color.Transparent;
+            this.iLstAspect.Images.SetKeyName(0, "makeverticle.png");
+            this.iLstAspect.Images.SetKeyName(1, "makehorizontal.png");
+            // 
+            // btnAspect
+            // 
+            this.btnAspect.BackColor = System.Drawing.Color.Wheat;
+            this.btnAspect.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAspect.ForeColor = System.Drawing.Color.Transparent;
+            this.btnAspect.Location = new System.Drawing.Point(395, 50);
+            this.btnAspect.Name = "btnAspect";
+            this.btnAspect.Size = new System.Drawing.Size(36, 36);
+            this.btnAspect.TabIndex = 0;
+            this.btnAspect.UseVisualStyleBackColor = false;
+            this.btnAspect.Click += new System.EventHandler(this.btnAspect_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Font = new System.Drawing.Font("Wingdings", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.btnLeft.Location = new System.Drawing.Point(395, 30);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(18, 20);
+            this.btnLeft.TabIndex = 1;
+            this.ttAll.SetToolTip(this.btnLeft, "Move rule one pixel left.");
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Font = new System.Drawing.Font("Wingdings", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.btnRight.Location = new System.Drawing.Point(413, 30);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(18, 20);
+            this.btnRight.TabIndex = 2;
+            this.ttAll.SetToolTip(this.btnRight, "Move rule one pixel right.");
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Font = new System.Drawing.Font("Wingdings", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.btnUp.Location = new System.Drawing.Point(431, 49);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(20, 19);
+            this.btnUp.TabIndex = 3;
+            this.btnUp.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.ttAll.SetToolTip(this.btnUp, "Move rule one pixel up.");
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Font = new System.Drawing.Font("Wingdings", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.btnDown.Location = new System.Drawing.Point(431, 67);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(20, 19);
+            this.btnDown.TabIndex = 4;
+            this.btnDown.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.ttAll.SetToolTip(this.btnDown, "Move rule one pixel down.");
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // ttAll
+            // 
+            this.ttAll.IsBalloon = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Wingdings 2", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.btnClose.Location = new System.Drawing.Point(457, 58);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(20, 20);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "S";
+            this.btnClose.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.ttAll.SetToolTip(this.btnClose, "Move rule one pixel up.");
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Wheat;
+            this.ClientSize = new System.Drawing.Size(800, 125);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnAspect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmMain_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseUp);
+            this.Resize += new System.EventHandler(this.frmMain_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList iLstAspect;
+        private System.Windows.Forms.Button btnAspect;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.ToolTip ttAll;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ PixelRuler/frmMain.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace PixelRuler
+{
+    public partial class frmMain : Form
+    {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private bool IAmVertical = false;
+        private bool doItOnce = false;
+        private Rectangle rectLeft;
+        private Rectangle rectRight;
+        private Rectangle rectUp;
+        private Rectangle rectDown;
+        private Rectangle rectCenter;
+        private Point MousePos;
+        private direction resizeDirection;
+        private Point resizingStart;
+        private int minWidth = 150;
+        private int minHeight = 150;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_MouseDown(object sender, MouseEventArgs e)
+        {
+            resizeDirection = direction.none;
+            if (e.Button != MouseButtons.Left) { return; }
+            
+            resizingStart = PointToScreen(new Point(e.X, e.Y));
+
+            if (rectLeft.Contains(MousePos))
+            {
+                resizeDirection = direction.left;
+            }
+            else if (rectRight.Contains(MousePos))
+            {
+                resizeDirection = direction.right;
+            }
+            else if (rectUp.Contains(MousePos))
+            {
+                resizeDirection = direction.up;
+            }
+            else if (rectDown.Contains(MousePos))
+            {
+                resizeDirection = direction.down;
+            }
+            else
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+                executeresize(); //redraw in case off screen erase
+            }
+            
+        }
+        
+        private void executeresize()
+        {
+            int wd = this.Size.Width;
+            int ht = this.Size.Height;
+            int controlWidth = btnAspect.Width + btnUp.Width;
+            int controlHeight = btnAspect.Height + btnLeft.Height;
+            //set the aspect control in center
+            btnAspect.Image = (IAmVertical ? iLstAspect.Images[1] : iLstAspect.Images[0]);
+            Point buttonPoint = new Point();
+            buttonPoint.X = (IAmVertical ? ((int)Math.Floor(wd / 2D) - (int)Math.Floor(controlWidth / 2D)) : 50 - (int)Math.Floor(controlWidth/2D));
+            buttonPoint.Y = (IAmVertical ? 50 - (int)Math.Floor(controlHeight / 2D) + btnLeft.Height : ((int)Math.Floor(ht / 2D) - (int)Math.Floor(controlHeight / 2D)) + btnLeft.Height);
+            btnAspect.Location = buttonPoint;
+            btnLeft.Location = new Point(btnAspect.Location.X, btnAspect.Location.Y - btnLeft.Height);
+            btnRight.Location = new Point(btnLeft.Location.X + btnLeft.Width, btnLeft.Location.Y);
+            btnUp.Location = new Point(btnAspect.Location.X + btnAspect.Width, btnAspect.Location.Y);
+            btnDown.Location = new Point(btnUp.Location.X, btnUp.Location.Y + btnUp.Height);
+            btnLeft.Text = char.ConvertFromUtf32(0xEF);
+            btnRight.Text = char.ConvertFromUtf32(0xF0);
+            btnUp.Text = char.ConvertFromUtf32(0xF1);
+            btnDown.Text = char.ConvertFromUtf32(0xF2);
+            btnClose.Location = new Point(wd - 40, (IAmVertical ? 20 : 40));
+
+            ttAll.SetToolTip(btnAspect, (IAmVertical ? "Click to make ruler horizontal." : "Click to make ruler vertical"));
+            ttAll.SetToolTip(btnClose, "Close the ruler");
+
+            int pos = 0;
+            int dim = (IAmVertical ? ht : wd);
+            tickType tt;
+            Graphics g = this.CreateGraphics();
+
+            Pen pen = new Pen(Color.Black);
+            g.Clear(Color.Wheat);
+            while (pos < dim)
+            {
+                if (pos > 0)
+                {
+                    if (pos % 100 == 0)
+                    {
+                        tt = tickType.century;
+                    }
+                    else if (pos % 50 == 0)
+                    {
+                        tt = tickType.quint;
+                    }
+                    else if (pos % 10 == 0)
+                    {
+                        tt = tickType.decade;
+                    }
+                    else
+                    {
+                        tt = tickType.fifth;
+                    }
+                    drawTick(pos, tt);
+                }
+                pos += 5;
+            }
+            //set rectangle handles
+            int handleThickness = 4;
+            int handleHt = 50;
+            rectLeft = new Rectangle(0, (ht - handleHt) / 2, handleThickness, handleHt);
+            rectRight = new Rectangle(wd - handleThickness - 1, (ht - handleHt) / 2, handleThickness, handleHt);
+            rectUp = new Rectangle((wd - handleHt) / 2, 0, handleHt, handleThickness);
+            rectDown = new Rectangle((wd - handleHt) / 2, ht - handleThickness - 1, handleHt, handleThickness);            
+            if (IAmVertical)
+            {
+                g.DrawRectangle(Pens.Black, rectUp);
+                g.DrawRectangle(Pens.Black, rectDown);
+                rectCenter = new Rectangle(0, 100, wd, ht - 100 - handleThickness);
+            }
+            else
+            {
+                g.DrawRectangle(Pens.Black, rectLeft);
+                g.DrawRectangle(Pens.Black, rectRight);
+                rectCenter = new Rectangle(100, 0, wd - 100 - handleThickness, ht);
+            }
+            g.DrawRectangle(Pens.Transparent, rectCenter);
+
+            g.Dispose();
+        }
+        private enum tickType
+        {
+            century,
+            decade,
+            quint,
+            fifth
+        }
+        private void drawTick(int pos, tickType _tickType)
+        {
+            int wd = this.Size.Width;
+            int ht = this.Size.Height;
+            //Rulers: 
+            //  horizontal: ruler1 = top, ruler2 = bottom
+            //  vertical: ruler1 = right, ruler2 = left
+            int lineLenght = 0;
+            Font ft = new Font("Arial", 20, GraphicsUnit.Pixel);
+            switch (_tickType)
+            {
+                case tickType.century:
+                    lineLenght = 15;
+                    ft = new Font("Arial", 20, GraphicsUnit.Pixel);
+                    break;
+                case tickType.quint:
+                    lineLenght = 12;
+                    ft = new Font("Arial", 12, GraphicsUnit.Pixel);
+                    break;
+                case tickType.decade:
+                    lineLenght = 5;
+                    ft = new Font("Arial", 8, GraphicsUnit.Pixel);
+                    break;
+                default:
+                    lineLenght = 3;
+                    break;
+            }
+            Graphics g = this.CreateGraphics();
+            Rectangle ruler1 = new Rectangle();
+            Rectangle ruler2 = new Rectangle();
+            Point textPoint1 = new Point();
+            Point textPoint2 = new Point();
+            int adjPost = pos;
+            if (adjPost > 100 && adjPost % 100 != 0)
+            {
+                adjPost -= (adjPost - (adjPost % 100));
+            }
+            SizeF stringSize = g.MeasureString(adjPost.ToString("#"), ft);
+            if (IAmVertical)
+            {
+                ruler1 = new Rectangle(wd - lineLenght, pos, lineLenght, 0);
+                ruler2 = new Rectangle(0, pos, lineLenght, 0);
+                textPoint1 = new Point((int)(wd - lineLenght - stringSize.Width - 2), (int)Math.Floor(pos - (stringSize.Height / 2)));
+                textPoint2 = new Point((int)(lineLenght + 2), (int)Math.Floor(pos - (stringSize.Height / 2)));
+            }
+            else
+            {
+                ruler1 = new Rectangle(pos, 0, 0, lineLenght);
+                ruler2 = new Rectangle(pos, ht - lineLenght, 0, lineLenght);
+                textPoint1 = new Point((int)Math.Floor(pos - (stringSize.Width / 2)), (int)(lineLenght + 2));
+                textPoint2 = new Point((int)Math.Floor(pos - (stringSize.Width / 2)), (int)(ht - lineLenght - 2 - stringSize.Height));
+            }
+            Pen pen = new Pen(Color.Black);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+            g.DrawLine(pen, ruler1.X, ruler1.Y, ruler1.X + ruler1.Width, ruler1.Y + ruler1.Height);
+            g.DrawLine(pen, ruler2.X, ruler2.Y, ruler2.X + ruler2.Width, ruler2.Y + ruler2.Height);
+            if (_tickType != tickType.fifth)
+            {
+                g.DrawString(adjPost.ToString("#"), ft, drawBrush, textPoint1);
+                g.DrawString(adjPost.ToString("#"), ft, drawBrush, textPoint2);
+            }
+            g.Dispose();
+        }
+        
+        private void frmMain_Paint(object sender, PaintEventArgs e)
+        {
+            if (!doItOnce)
+            {
+                executeresize();
+                doItOnce = true;
+            }
+        }
+
+        private void btnAspect_Click(object sender, EventArgs e)
+        {
+            IAmVertical = !IAmVertical;
+            int currentWD = this.Width;
+            int currentHt = this.Height;
+            this.Width = currentHt;
+            this.Height = currentWD;
+            executeresize();
+        }
+        private enum direction
+        {
+            up,
+            down,
+            left,
+            right,
+            none
+        };
+        private void move(direction dir)
+        {
+
+            int currentX = this.Location.X;
+            int currentY = this.Location.Y;
+            switch (dir)
+            {
+                case direction.down:
+                    currentY += 1;
+                    break;
+                case direction.left:
+                    currentX -= 1;
+                    break;
+                case direction.right:
+                    currentX += 1;
+                    break;
+                case direction.up:
+                    currentY -= 1;
+                    break;
+            }
+            this.Location = new Point(currentX, currentY);
+        }
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            move(direction.left);
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            move(direction.right);
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            move(direction.up);
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            move(direction.down);
+        }
+
+        private void frmMain_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point resizingEnding = PointToScreen(new Point(e.X, e.Y));
+            Rectangle rectForm = Bounds;
+            if (Capture)
+            {
+                //this means that the form has capture the mouse as in resizing, not during move
+                if (resizeDirection != direction.none)
+                {
+                    switch (resizeDirection)
+                    {
+                        case direction.up:
+                            Bounds = new Rectangle(rectForm.X, rectForm.Y + resizingEnding.Y - resizingStart.Y, rectForm.Width, rectForm.Height - resizingEnding.Y + resizingStart.Y);
+                            break;
+                        case direction.right:
+                            Bounds = new Rectangle(rectForm.X, rectForm.Y, rectForm.Width + resizingEnding.X - resizingStart.X, rectForm.Height);
+                            break;
+                        case direction.down:
+                            Bounds = new Rectangle(rectForm.X, rectForm.Y, rectForm.Width, rectForm.Height + resizingEnding.Y - resizingStart.Y);
+                            break;
+                        case direction.left:
+                            Bounds = new Rectangle(rectForm.X + resizingEnding.X - resizingStart.X, rectForm.Y, rectForm.Width - resizingEnding.X + resizingStart.X, rectForm.Height);
+                            break;
+                    }
+                    resizingStart = resizingEnding;
+                    doItOnce = false;
+                    Invalidate();
+                }
+            }
+            else
+            {
+                MousePos = new Point(e.X, e.Y);
+                Cursor = Cursors.Default;
+                if (rectCenter.Contains(MousePos))
+                {
+                    Cursor = Cursors.SizeAll;
+                }
+                if (rectUp.Contains(MousePos) || rectDown.Contains(MousePos))
+                {
+                    Cursor = Cursors.SizeNS;
+                }
+                if (rectLeft.Contains(MousePos) || rectRight.Contains(MousePos))
+                {
+                    Cursor = Cursors.SizeWE;
+                }
+            }
+        }
+
+        private void frmMain_MouseUp(object sender, MouseEventArgs e)
+        {
+            executeresize();
+        }
+
+        private void frmMain_Resize(object sender, EventArgs e)
+        {
+            if (Height < minHeight)
+            {
+                Capture = false;
+                Height = minHeight;
+            }
+            if (Width < minWidth)
+            {
+                Capture = false;
+                Width = minWidth;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
